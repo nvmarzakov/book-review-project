12158dfbb0110db2f9bec788150ef792f79bf3fc
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2025-03-11 23:21:04 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel + Bootstrap 5 + Vite</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container mt-5">
+        
+    </div>
+</body>
+</html>
